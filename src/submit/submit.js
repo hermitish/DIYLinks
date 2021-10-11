ac60404791f75b5215b5fee831dc6f16e07f5ebe
hermitish <87009448+hermitish@users.noreply.github.com>
2021-10-11 18:57:28 +0530

    Add submit.js

---
+++ src/submit/submit.js
@@ -0,0 +1,30 @@
+import { firebaseApp } from '../initialise.js';
+import { getFirestore, collection, addDoc } from 'https://www.gstatic.com/firebasejs/9.1.0/firebase-firestore.js';
+// https://www.gstatic.com/firebasejs/9.1.0/firebase-firestore.js
+
+if(localStorage.getItem('userLoggedIn') == 'True'){
+    const db = getFirestore();   
+    const post_form = document.getElementById("post-submit");
+    post_form.addEventListener('submit', e => {
+        e.preventDefault();        
+        try {
+            const poster_email = localStorage.getItem('userEmail');
+            setTimeout(() => {
+                const postDocRef = addDoc(collection(db, "posts"), {
+                    title: post_form['title-input']['value'],
+                    url: post_form['url-input']['value'],
+                    votes: 0,
+                    poster_email: poster_email
+                }).then(() => {
+                    window.location.href = "../index.html";
+                }).catch((e) => {
+                    console.log("Error submitting post: ", e);
+                })                
+            }, 3)            
+        } catch (e) {
+            console.log("Error submitting post: ", e);
+        }
+    })
+} else {
+    window.location.href = "../login/login.html";
+}
