288abb885edae8c07a59d58c538a1af8dee0ca2d
hermitish <87009448+hermitish@users.noreply.github.com>
2021-10-13 13:58:13 +0530

    Update index.js to show post submitter username

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // 'https://www.gstatic.com/firebasejs/9.1.0/firebase-auth.js';
-import { doc, getFirestore, getDocs, query, collection, updateDoc } from 'https://www.gstatic.com/firebasejs/9.1.0/firebase-firestore.js';
+import { doc, getFirestore, getDocs, query, collection, updateDoc, orderBy, limit } from 'https://www.gstatic.com/firebasejs/9.1.0/firebase-firestore.js';
 import { signOut } from 'https://www.gstatic.com/firebasejs/9.1.0/firebase-auth.js';
 import { firebaseApp, auth } from './initialise.js';
 
@@ -10,7 +10,7 @@ const itemList = document.getElementById("itemlist");
 
 var count = 1
 
-const q = query(collection(db, "posts"));
+const q = query(collection(db, "posts"), orderBy("votes", "desc"), limit(25));
 
 const querySnapshot = await getDocs(q);
 querySnapshot.forEach((doc1) => {    
@@ -20,7 +20,8 @@ querySnapshot.forEach((doc1) => {
     var td1 = document.createElement("td");    
     var td2 = document.createElement("td");
     var upArrow = String.fromCharCode(9652);
-    var upArrowButton = document.createElement("button");    
+    var upArrowButton = document.createElement("button");
+    var posterShow = document.createElement("td");
     upArrowButton.innerText = upArrow;
     upArrowButton.style.color = "green";
     upArrowButton.onclick = function () {
@@ -42,8 +43,14 @@ querySnapshot.forEach((doc1) => {
         displayUrl = displayUrl.concat("...");
     }
     td2.innerHTML = "<a href=" + docData['url'] + " target=\"_blank\">" + displayUrl + "</a>";    
+    if(docData['poster_username'] != undefined){
+        posterShow.innerText = docData['poster_username']; // Eventually a link to the user's profile[1] 
+    } else {
+        posterShow.innerText = docData['poster_email']; // Eventually a link to the user's profile[1] 
+    }
     tr.appendChild(td1);
     tr.appendChild(td2);    
+    tr.appendChild(posterShow);
     itemList.appendChild(tr);    
     count += 1;
 })
@@ -63,3 +70,5 @@ logout_button.addEventListener('click', e => {
     e.preventDefault();
     logOut();
 })
+
+// [1]: A user's profile should show the past submissions, points(TBD), and possibly other things.
